20211b7729690283b780bb4e8501c1eb3e06d2f5
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-09 22:47:46 -0700

    Modify CommissionEmployee.java to extend Employee.java

--- CommissionEmployee.java
+++ CommissionEmployee.java
@@ -1,16 +1,17 @@
 /*
- * Commission Employee Superclass
- * Based On "Java How To Program, 10, Early Objects" - Chapter 9.
+ * Commission Employee subclass
+ * Used to demonstrate Polymorphism and Inheritance
+ * Based On "Java How To Program, 10, Early Objects" - Chapter 10.
  */
 
-public class CommissionEmployee {
-    private final String firstName;
-    private final String lastName;
-    private final String ssNumber;
+public class CommissionEmployee extends Employee {
+
     private double grossSales;
     private double commissionRate;
     
     public CommissionEmployee(String nFirstName, String nLastName, String nssNumber, double nGrossSales, double nCommissionRate) {
+        super(nFirstName, nLastName, nssNumber);
+        
         if (nGrossSales < 0.0) {
             throw new IllegalArgumentException("Gross sales must exceed $0");
         }
@@ -19,36 +20,22 @@ public class CommissionEmployee {
             throw new IllegalArgumentException ("Commission rate must be between 0.0 and 1.0");
         }
         
-        this.firstName = nFirstName;
-        this.lastName  = nLastName;
-        this.ssNumber = nssNumber;
         this.commissionRate = nCommissionRate;
+        this.grossSales = nGrossSales;
     }//End Constructor
     
     //Setters and Getters
-    public String getFirstName() {
-        return firstName;
-    }
-    
-    public String getLastName() {
-        return lastName;
-    }
-    
-    public String getSS() {
-        return ssNumber;
-    }
-    
     public void setGross(double nGrossSales) {
         if (nGrossSales < 0.0) {
             throw new IllegalArgumentException("Gross sales must exceed $0");
         }
         
         this.grossSales = nGrossSales;
-    }
+    }//End setGross
     
-    public double getGrossSales() {
+    public double getGross() {
         return grossSales;
-    }
+    }//End getGross
     
     public void setCommission(double nCommissionRate) {
         if (nCommissionRate <= 0.0 || nCommissionRate >= 1.0) {
@@ -56,36 +43,29 @@ public class CommissionEmployee {
         }
         
         commissionRate = nCommissionRate;
-    }
+    }//End setCommission
     
     public double getCommission() {
         return commissionRate;
-    }
+    }//End getCommission
     
+    @Override
     public double getEarnings() {
-        return getCommission() * getGrossSales();
-    }
+        return getCommission() * getGross();
+    }//End getEarnings
     
     @Override
     public String toString() {
-        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f", "Commission", getFirstName(), getLastName(),
-                "SS Number", getSS(), "Gross Sales", getGrossSales(), "Commission Rate", getCommission());
-    }
+        return String.format("%s: %s %s%n%s: %s%n%s: $%.2f%n%s: %.2f", "Commission", getFirstName(), getLastName(),
+                "SS Number", getSS(), "Gross Sales", getGross(), "Commission Rate", getCommission());
+    }//end toString
     
-    
+    //CommissionEmployee test
     public static void main(String[] args) {
        CommissionEmployee emp = new CommissionEmployee("Bob", "Smith", "111-11-111",
        100000, .06);
-       
-       System.out.printf("%s %s%n","First name: ", emp.getFirstName());
-       System.out.printf("%s %s%n","Last name: ", emp.getLastName());
-       System.out.printf("%s %s%n","SS Number: ", emp.getSS());
-       System.out.printf("%s %s%n","Gross Sales: ", emp.getGrossSales());
-       System.out.printf("%s %s%n","Commission Rate: ", emp.getCommission());
-       
-       emp.setGross(5000);
-       emp.setCommission(.1);
-       System.out.printf("%n%s:%n%s%n","From toString()",emp);
+
+       System.out.printf("%n%s:%n%s%n%s $%.2f%n","From toString()",emp, "Earnings", emp.getEarnings());
     }//End Main
     
 }//End CommissionEmployee
