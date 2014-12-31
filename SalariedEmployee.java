4f7ae5272c93492003aeebc9e978873f1e10d355
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-30 17:36:09 -0900

    Fix formatting based on checkstyle

--- SalariedEmployee.java
+++ SalariedEmployee.java
@@ -8,49 +8,51 @@ public class SalariedEmployee extends Employee {
     private double weeklySalary;
     
     //Constructor
-    public SalariedEmployee(String nFirstName, String nLastName, String nssNumber,
-            double nWeeklySalary) {
+    public SalariedEmployee(String nFirstName, String nLastName, 
+            String nssNumber, double nWeeklySalary) {
         super(nFirstName, nLastName, nssNumber); //Call superclass constructor
         
-        if(nWeeklySalary < 0.0) {
-            throw new IllegalArgumentException("Weekly salary must be positive");
+        if (nWeeklySalary < 0.0) {
+            throw new IllegalArgumentException
+                ("Weekly salary must be positive");
         }
         
         this.weeklySalary = nWeeklySalary;
-    }//End Consturctor
+    } //End Consturctor
     
     //Set and Get
     public void setSalary(double nWeeklySalary) {
-        if(nWeeklySalary < 0.0) { //Calidate weekly salary
-            throw new IllegalArgumentException("Weekly salary must be positive");
+        if (nWeeklySalary < 0.0) { //Calidate weekly salary
+            throw new IllegalArgumentException
+                ("Weekly salary must be positive");
         }
 
         this.weeklySalary = nWeeklySalary;
-    }//End setSalary
+    } //End setSalary
     
     public double getSalary() {
         return weeklySalary;
-    }//End getSalary
+    } //End getSalary
     
     @Override
     public double getPaymentAmount() {
         return getSalary();
-    }//End getPaymentAmount
+    } //End getPaymentAmount
     
     
     @Override
     public String toString() {
         return String.format("%s%n%s: $%.2f", super.toString(), 
                 "Salary", getSalary());
-    }//End toString
+    } //End toString
     
     
     //SalariedEmployee test
     public static void main(String[] args) {
-       SalariedEmployee emp = new SalariedEmployee("Jack", "Dangerfield", "333-33-333",
-               3000);
+       SalariedEmployee emp = new SalariedEmployee("Jack", "Dangerfield", 
+               "333-33-333", 3000);
        
-       System.out.printf("%s:%n%s%n","From toString()",emp);
-    }//End Main
+       System.out.printf("%s:%n%s%n", "From toString()", emp);
+    } //End Main
     
-}//End SalariedEmployee
+} //End SalariedEmployee
