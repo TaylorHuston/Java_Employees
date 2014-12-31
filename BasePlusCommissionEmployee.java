2cdcdb6fb49c7c3486e9d2e00119050d0827b980
Taylor Huston <taylor.a.huston@gmail.com>
2014-12-30 18:28:14 -0900

    Fix formatting based on checkstyle

--- BasePlusCommissionEmployee.java
+++ BasePlusCommissionEmployee.java
@@ -8,47 +8,54 @@ public class BasePlusCommissionEmployee extends CommissionEmployee {
     private double baseSalary;
     
     //Constructor
-    public BasePlusCommissionEmployee(String nFirstName, String nLastName, String nssNumber,
-        double nGrossSales, double nCommissionRate, double nBaseSalary) {
-        super(nFirstName, nLastName, nssNumber, nGrossSales, nCommissionRate); //Call superclass constructor
+    public BasePlusCommissionEmployee(String nFirstName, String nLastName, 
+            String nssNumber, double nGrossSales, double nCommissionRate, 
+            double nBaseSalary) {
+        
+        //Call superclass constructor
+        super(nFirstName, nLastName, nssNumber, nGrossSales, nCommissionRate); 
 
-        if (nBaseSalary < 0.0) { //Validate and set base salary
+        //Validate and set base salary
+        if (nBaseSalary < 0.0) { 
             throw new IllegalArgumentException("Base salary must be positive.");
         }
         
         this.baseSalary = nBaseSalary;
-    }
+    } //End Constructor
     
     //Setters and Getters
     public void setBaseSalary(double nBaseSalary) {
-        if (nBaseSalary < 0.0) { //Validate base salary
+        //Validate base salary
+        if (nBaseSalary < 0.0) { 
             throw new IllegalArgumentException("Base salary must be positive.");
         }
         
         this.baseSalary = nBaseSalary;
-    }//End setBaseSalary
+    } //End setBaseSalary
     
     public double getBaseSalary() {
         return baseSalary;
-    }//End getBaseSalary
+    } //End getBaseSalary
     
     @Override
     public double getPaymentAmount() {
         return getBaseSalary() + super.getPaymentAmount();
-    }//End getPaymentAmount
+    } //End getPaymentAmount
     
     @Override
     public String toString() {
-        return String.format("%s%n%s: $%.2f", super.toString(), 
-                "Base Salary", getBaseSalary());   
-    }//End toString
+        return String.format("%s%n%s: $%.2f", super.toString(), "Base Salary", 
+                getBaseSalary());   
+    } //End toString
     
     //BasePlusCommissionEmployee Test
     public static void main(String[] args) {
-       BasePlusCommissionEmployee emp = new BasePlusCommissionEmployee("Jane", "Smith", 
-               "222-22-222", 70000, .04, 500);
+       BasePlusCommissionEmployee emp = 
+               new BasePlusCommissionEmployee("Jane", "Smith", "222-22-222", 
+                       70000, .04, 500);
   
-       System.out.printf("%s:%n%s%n%s: $%.2f%n","From toString()",emp, "Earnings", emp.getPaymentAmount());
-    }//End Main 
+       System.out.printf("%s:%n%s%n%s: $%.2f%n", "From toString()", 
+               emp, "Earnings", emp.getPaymentAmount());
+    } //End main 
     
-}//End BasePlusCommissionEmployee
+} //End BasePlusCommissionEmployee
